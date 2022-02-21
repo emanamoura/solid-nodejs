1e7383a13ff699bd585713007e32628c668a6c5d
Emanuel Moura <emanuel.moura@ccc.ufcg.edu.br>
2022-02-21 13:48:27 -0300

    Implementing user controller and user use case to show all users in the application

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
@@ -6,7 +7,13 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.header('user_id');
+    try {
+      const user = this.listAllUsersUseCase.execute({user_id});
+      return response.status(200).json(user);
+    }catch(err) {
+      return response.status(400).json({error: err.message})
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -9,7 +10,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    const isAdminUser = user.admin;
+    if(!user) {
+      throw new Error("User doesn't exists!");
+    }
+    if(!isAdminUser) {
+      throw new Error("User doesn't have permissions!");
+    }
+    return this.usersRepository.list();
   }
 }
 
